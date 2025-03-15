b9d7cc8cbf771d30e10f3c660c4addd9168d99b8
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-03-15 14:41:53 +0100

    add types for props

--- src/components/IconPlusSpanDiv.tsx
+++ src/components/IconPlusSpanDiv.tsx
@@ -4,13 +4,16 @@ import styled from "styled-components";
 type IconPlusSpanDivPropsType = {
     iconId: string
     spanText: string
+    width?: string
+    height?: string
+    viewBox?: string
 }
 
 
 export const IconPlusSpanDiv = (props: IconPlusSpanDivPropsType) => {
     return (
         <StyledIconPlusSpanDiv>
-            <Icon iconId={props.iconId} />
+            <Icon iconId={props.iconId} width={props.width} height={props.height} viewBox={props.viewBox} />
             <SpanForIconPlusSpanDiv>{props.spanText}</SpanForIconPlusSpanDiv>
         </StyledIconPlusSpanDiv>
     );
