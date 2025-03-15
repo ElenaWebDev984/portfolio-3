9a1c88690dcf633b1f71d55f8c5ff906252e65a9
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-03-15 18:58:21 +0100

    create Container

---
+++ src/components/Container.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    max-width: 1230px;
+    width: 100%;
+    //min-height: 100vh;
+    padding: 15px;
+    margin: 0 auto;
+    outline: 1px solid red;
+`
