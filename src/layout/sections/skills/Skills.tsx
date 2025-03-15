e68c9bc54c15123a090244619988565c324353a3
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-03-15 14:13:26 +0100

    change view for icons

--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -11,18 +11,18 @@ export const Skills = () => {
             <SectionTitle>My Tech Stack</SectionTitle>
             <SectionText>Technologies I’ve been working with recently</SectionText>
             <FlexWrapper wrap="wrap" gap={'80px 105px'}>
-                <Icon iconId={'html'} viewBox={'0 0 120 120'} />
-                <Icon iconId={'css'} viewBox={'0 0 120 120'} />
-                <Icon iconId={'js'} viewBox={'0 0 140 140'} />
-                <Icon iconId={'ts'} viewBox={'0 0 140 140'} />
-                <Icon iconId={'react'} viewBox={'0 0 120 120'} />
-                <Icon iconId={'styledComponents'} viewBox={'0 0 140 140'} />
-                <Icon iconId={'github'} viewBox={'0 0 100 100'} />
-                <Icon iconId={'git'} viewBox={'0 0 110 110'} />
-                <Icon iconId={'figma'} viewBox={'0 0 130 130'} />
-                <Icon iconId={'webstorm'} viewBox={'0 0 100 100'} />
-                <Icon iconId={'pnpm'} viewBox={'0 0 120 120'} />
-                <Icon iconId={'vite'} viewBox={'0 0 220 220'} />
+                <Icon iconId={'html'} viewBox={'0 -4 120 120'} />
+                <Icon iconId={'css'} viewBox={'0 -4 120 120'} />
+                <Icon iconId={'js'} viewBox={'-10 -10 150 150'} />
+                <Icon iconId={'ts'} viewBox={'-10 -10 150 150'} />
+                <Icon iconId={'react'} viewBox={'-10 -10 130 130'} />
+                <Icon iconId={'styledComponents'} viewBox={'-12 -12 150 150'} />
+                <Icon iconId={'github'} viewBox={'-5 -5 100 100'} />
+                <Icon iconId={'git'} viewBox={'-8 -8 120 120'} />
+                <Icon iconId={'figma'} viewBox={'-10 -10 150 150'} />
+                <Icon iconId={'webstorm'} viewBox={'0 0 120 120'} />
+                <Icon iconId={'pnpm'} viewBox={'2 2 120 120'} />
+                <Icon iconId={'vite'} viewBox={'4 4 240 240'} />
             </FlexWrapper>
         </StyledSkills>
     );
