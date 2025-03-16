4d14669b969f9a635688aab7263da95b682463e6
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-03-16 16:49:30 +0100

    change Menu on HeaderMenu

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 import {Logo} from "../../components/logo/Logo.tsx";
-import {Menu} from "../../components/menu/Menu.tsx";
 import {MenuContact} from "../../components/menu/MenuContact.tsx";
 import {FlexWrapper} from "../../components/FlexWrapper.tsx";
 import {items} from "../header/Header.tsx";
 import {Container} from "../../components/Container.tsx";
+import {HeaderMenu} from "../header/headerMenu/HeaderMenu.tsx";
 
 
 export const Footer = () => {
@@ -16,7 +16,7 @@ export const Footer = () => {
                    <MenuContact/>
                </FlexWrapper>
                <FlexWrapper justifyContent="space-between">
-                   <Menu menuItems={items}/>
+                   <HeaderMenu menuItems={items}/>
                    <StyledFooterText>© 2025 Designed and built by <span>Elena</span> with <span>Love</span> & <span>Coffee</span></StyledFooterText>
                </FlexWrapper>
            </Container>
