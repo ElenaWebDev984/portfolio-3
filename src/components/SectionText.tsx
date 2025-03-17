d1657bbcb54e62dfd561d2ef8a0f7df386b29cb2
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-03-18 00:07:40 +0100

    add styles

--- src/components/SectionText.tsx
+++ src/components/SectionText.tsx
@@ -1,5 +1,10 @@
 import styled from "styled-components";
+import {theme} from "../styles/Theme.ts";
 
 export const SectionText = styled.p`
     text-align: center;
+    font-weight: 400;
+    font-size: 32px;
+    color: ${theme.colors.linkNavFont};
+    margin: 50px 0;
 `
