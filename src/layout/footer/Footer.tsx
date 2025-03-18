e5bf7bc135757e2bf4c7fa268113a20c479b3a6a
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-03-18 22:32:06 +0100

    add line (pseudoWrapper)

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -5,6 +5,7 @@ import {FlexWrapper} from "../../components/FlexWrapper.tsx";
 import {items} from "../header/Header.tsx";
 import {Container} from "../../components/Container.tsx";
 import {HeaderMenu} from "../header/headerMenu/HeaderMenu.tsx";
+import {theme} from "../../styles/Theme.ts";
 
 
 export const Footer = () => {
@@ -15,6 +16,7 @@ export const Footer = () => {
                    <Logo/>
                    <MenuContact/>
                </FlexWrapper>
+               <DivPseudoWrapper/>
                <FlexWrapper justifyContent="space-between">
                    <HeaderMenu menuItems={items}/>
                    <StyledFooterText>© 2025 Designed and built by <span>Elena</span> with <span>Love</span> & <span>Coffee</span></StyledFooterText>
@@ -29,5 +31,27 @@ const StyledFooter = styled.footer`
 `
 
 const StyledFooterText = styled.p`
+    font-weight: 400;
+    font-size: 18px;
 
+    span {
+        background: ${theme.colors.linearGradientFont};
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        background-clip: text;
+        color: transparent;
+        display: inline-block;
+    }
+`
+
+const DivPseudoWrapper = styled.div`
+    &::before {
+        content: '';
+        display: inline-block;
+        width: 100%;
+        height: 2px;
+        background-color: ${theme.colors.borderFooter};
+        opacity: 30%;
+        margin: 30px 0;
+    }
 `
