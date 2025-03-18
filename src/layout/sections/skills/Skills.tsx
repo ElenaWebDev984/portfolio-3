bd0cf6050d445673f417a6fa82a3f0b2e372d7a3
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-03-18 22:34:40 +0100

    add flexWrapperGrid

--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -1,9 +1,9 @@
 import styled from "styled-components";
-import {FlexWrapper} from "../../../components/FlexWrapper.tsx";
 import {Icon} from "../../../components/icon/Icon.tsx";
 import {SectionTitle} from "../../../components/SectionTitle.tsx";
 import {SectionText} from "../../../components/SectionText.tsx";
 import {Container} from "../../../components/Container.tsx";
+import { FlexWrapperGrid } from "../../../components/FlexWrapperGrid.tsx";
 
 
 export const Skills = () => {
@@ -12,20 +12,20 @@ export const Skills = () => {
             <Container>
                 <SectionTitle>My Tech Stack</SectionTitle>
                 <SectionText>Technologies I’ve been working with recently</SectionText>
-                <FlexWrapper wrap="wrap" gap={'80px 105px'}>
-                    <Icon iconId={'html'} width={'120px'} height={'120px'} viewBox={'0 -4 120 120'} />
-                    <Icon iconId={'css'} width={'120px'} height={'120px'} viewBox={'0 -4 120 120'} />
-                    <Icon iconId={'js'} width={'120px'} height={'120px'} viewBox={'-10 -10 150 150'} />
-                    <Icon iconId={'ts'} width={'120px'} height={'120px'} viewBox={'-10 -10 150 150'} />
-                    <Icon iconId={'react'} width={'120px'} height={'120px'} viewBox={'-10 -10 130 130'} />
-                    <Icon iconId={'styledComponents'} width={'120px'} height={'120px'} viewBox={'-12 -12 150 150'} />
-                    <Icon iconId={'github'} width={'120px'} height={'120px'} viewBox={'-5 -5 100 100'} />
-                    <Icon iconId={'git'} width={'120px'} height={'120px'} viewBox={'-8 -8 120 120'} />
-                    <Icon iconId={'figma'} width={'120px'} height={'120px'} viewBox={'-10 -10 150 150'} />
-                    <Icon iconId={'webstorm'} width={'120px'} height={'120px'} viewBox={'0 0 120 120'} />
-                    <Icon iconId={'pnpm'} width={'120px'} height={'120px'} viewBox={'2 2 120 120'} />
-                    <Icon iconId={'vite'} width={'120px'} height={'120px'} viewBox={'4 4 240 240'} />
-                </FlexWrapper>
+            <FlexWrapperGrid gap={'80px 100px'} justifyContent={'center'} alignItems={'center'}>
+                <Icon iconId={'html'} width={'120px'} height={'120px'} viewBox={'0 -4 120 120'} />
+                <Icon iconId={'css'} width={'120px'} height={'120px'} viewBox={'0 -4 120 120'} />
+                <Icon iconId={'js'} width={'120px'} height={'120px'} viewBox={'-10 -10 150 150'} />
+                <Icon iconId={'ts'} width={'120px'} height={'120px'} viewBox={'-10 -10 150 150'} />
+                <Icon iconId={'react'} width={'120px'} height={'120px'} viewBox={'-10 -10 130 130'} />
+                <Icon iconId={'styledComponents'} width={'120px'} height={'120px'} viewBox={'-12 -12 150 150'} />
+                <Icon iconId={'github'} width={'120px'} height={'120px'} viewBox={'-5 -5 100 100'} />
+                <Icon iconId={'git'} width={'120px'} height={'120px'} viewBox={'-8 -8 120 120'} />
+                <Icon iconId={'figma'} width={'120px'} height={'120px'} viewBox={'-10 -10 150 150'} />
+                <Icon iconId={'webstorm'} width={'120px'} height={'120px'} viewBox={'0 0 120 120'} />
+                <Icon iconId={'pnpm'} width={'120px'} height={'120px'} viewBox={'2 2 120 120'} />
+                <Icon iconId={'vite'} width={'120px'} height={'120px'} viewBox={'4 4 240 240'} />
+            </FlexWrapperGrid>
             </Container>
         </StyledSkills>
     );
