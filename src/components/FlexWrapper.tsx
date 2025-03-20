c39fc50b7b6165294c5f5ca9a355f09dccb9a7ba
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-03-20 13:23:17 +0100

    update props

--- src/components/FlexWrapper.tsx
+++ src/components/FlexWrapper.tsx
@@ -15,6 +15,6 @@ export const FlexWrapper = styled.div<FlexWrapperPropsType>`
     justify-content: ${props => props.justifyContent || 'flex-start'};
     align-items: ${props => props.alignItems || 'stretch'};
     flex-wrap: ${props => props.wrap || 'nowrap'};
-    gap: ${props => props.gap || '20px'};
+    gap: ${props => props.gap || undefined};
     height: 100%;
 `
