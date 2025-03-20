3f237254352f20c0f625b33b753305cef3f9035c
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-03-20 13:21:57 +0100

    add props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,24 @@
 import styled from "styled-components";
 import {theme} from "../styles/Theme.ts";
 
-export const Button = styled.button`
-    width: 150px;
-    height: 40px;
+type ButtonPropsType = {
+    width?: string
+    height?: string
+    padding?: string
+    marginBottom?: string
+}
+
+
+export const Button = styled.button<ButtonPropsType>`
+    width: ${props => props.width || undefined};
+    height: ${props => props.height || undefined};
+    padding: ${props => props.padding || undefined};
+    margin-bottom: ${props => props.marginBottom || undefined};
     font-weight: 600;
     font-size: 20px;
     cursor: pointer;
     background-image: ${theme.colors.linearGradientFont};
     color: ${theme.colors.h2h3Font};
+    border-radius: 5px;
+    white-space: nowrap;
 `
