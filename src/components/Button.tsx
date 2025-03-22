0eab0e1943951bb09b9df7d89b067177cc2742a5
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-03-22 18:29:14 +0100

    add animation

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,13 @@
-import styled from "styled-components";
 import {theme} from "../styles/Theme.ts";
+import {BtnAnimation, BtnAnimationOut} from "../styles/animations/Animations.tsx";
+import styled from "styled-components";
 
 type ButtonPropsType = {
     width?: string
     height?: string
     padding?: string
     marginBottom?: string
+    btnType?: 'primary' | 'outlined'
 }
 
 
@@ -16,11 +18,22 @@ export const Button = styled.button<ButtonPropsType>`
     margin-bottom: ${props => props.marginBottom || undefined};
     background-image: ${theme.colors.linearGradientFont};
     color: ${theme.colors.h2h3Font};
+    text-align: center;
     font-weight: 600;
     font-size: 20px;
     cursor: pointer;
     border-radius: 5px;
     white-space: nowrap;
+    border: none;
+
+    &:hover {
+        box-shadow: 40px 0 100px ${theme.colors.accentFont}, -40px 0 100px ${theme.colors.accentFont2};
+        animation: ${BtnAnimation} 2s ease-in-out;
+    }
+
+    &:not(:hover) {
+        animation: ${BtnAnimationOut} 2s ease-in-out;
+    } 
 `
 
 // width={'150px'} height={'40px'}
