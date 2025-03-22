12d9dfc9d6c220dbd9f443125c312e3220e8b085
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-03-22 21:28:50 +0100

    add GridWrapper

--- src/layout/sections/projects/Projects.tsx
+++ src/layout/sections/projects/Projects.tsx
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 import {SectionTitle} from "../../../components/SectionTitle.tsx";
 import {SectionText} from "../../../components/SectionText.tsx";
-import {FlexWrapper} from "../../../components/FlexWrapper.tsx";
 import {Project} from "./project/Project.tsx";
 import projectImg1 from '../../../assets/images/proj1.webp';
 import projectImg2 from '../../../assets/images/proj2.webp';
@@ -11,6 +10,7 @@ import projectImg5 from '../../../assets/images/proj5.webp';
 import projectImg6 from '../../../assets/images/proj6.webp';
 import {Container} from "../../../components/Container.tsx";
 import {TabMenu} from "./tabMenu/TabMenu.tsx";
+import {GridWrapper} from "../../../components/GridWrapper.tsx";
 
 
 export const tabItems = ['All', 'HTML', 'JavaScript', 'SASS', 'React',]
@@ -25,14 +25,14 @@ export const Projects = () => {
                 <SectionTitle>Projects</SectionTitle>
                 <SectionText>Things I’ve built so far</SectionText>
                 <TabMenu menuItems={tabItems}/>
-                <FlexWrapper justifyContent={'space-between'} alignItems={'flex-start'} wrap={'wrap'} gap={'60px 35px'}>
+                <GridWrapper gridTemplateColumns={'repeat(3, 1fr)'} gap={'60px 35px'} >
                     <Project title={'Project Tile goes here'} text={'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content'} src={projectImg1} alt={'project1'} />
                     <Project title={'Project Tile goes here'} text={'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content'} src={projectImg2} alt={'project2'} />
                     <Project title={'Project Tile goes here'} text={'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content'} src={projectImg3} alt={'project3'} />
                     <Project title={'Project Tile goes here'} text={'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content'} src={projectImg4} alt={'project4'} />
                     <Project title={'Project Tile goes here'} text={'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content'} src={projectImg5} alt={'project5'} />
                     <Project title={'Project Tile goes here'} text={'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content'} src={projectImg6} alt={'project6'} />
-                </FlexWrapper>
+                </GridWrapper>
             </Container>
         </StyledProjects>
     );
