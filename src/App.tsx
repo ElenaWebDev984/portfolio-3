9555c0be5e920e0210cef935306e018923f8d4c8
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-03-23 12:30:05 +0100

    add Particle

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,14 @@ import {Slogan} from "./layout/sections/slogan/Slogan.tsx";
 import {Testimony} from "./layout/sections/testimony/Testimony.tsx";
 import {Contact} from "./layout/sections/contact/Contact.tsx";
 import {Footer} from "./layout/footer/Footer.tsx";
+import {Particle} from "./components/particle/Particle.tsx";
 
 
 
 function App() {
     return (
         <div className="App">
+            <Particle />
             <Header/>
             <Main/>
             <About/>
