ad927cf95903c3cb7388b9059a372eae76498d7b
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-03-24 02:37:57 +0100

    created MobileMenuContact

---
+++ src/layout/header/mobileMenu/MobileMenuContact.tsx
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+import {Icon} from "../../../components/icon/Icon.tsx";
+
+
+
+export const MobileMenuContact = () => {
+    return (
+        <StyledMobileMenuContact>
+            <MenuContactList>
+                <MenuContactLink href="">
+                    <Icon iconId={'github-contacts'} width={'30'} height={'30'} viewBox={'0 0 30 30'}/>
+                </MenuContactLink>
+            </MenuContactList>
+            <MenuContactList>
+                <MenuContactLink href="">
+                    <Icon iconId={'linkedin'} width={'30'} height={'30'} viewBox={'0 0 30 30'} />
+                </MenuContactLink>
+            </MenuContactList>
+            <MenuContactList>
+                <MenuContactLink href="">
+                    <Icon iconId={'telegram'} width={'30'} height={'30'} viewBox={'0 0 30 30'} />
+                </MenuContactLink>
+            </MenuContactList>
+        </StyledMobileMenuContact>
+    );
+};
+
+
+const StyledMobileMenuContact = styled.ul`
+    display: flex;
+    align-items: center;
+    gap: 20px;
+`
+
+const MenuContactList = styled.li`
+    &:hover {
+        transform: translateY(-4px);
+    }
+`
+
+const MenuContactLink = styled.a`
+    
+`
+
+
