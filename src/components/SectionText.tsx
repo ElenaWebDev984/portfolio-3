28486952a75618cf719e934df52f3f99dd7caf2f
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-03-26 17:56:49 +0100

    added  mixin and margin-bottom

--- src/components/SectionText.tsx
+++ src/components/SectionText.tsx
@@ -4,7 +4,11 @@ import {font} from "../styles/Common.ts";
 
 export const SectionText = styled.p`
     text-align: center;
-    ${font({weight: 400, Fmax: 32, Fmin: 24})}
+    ${font({weight: 400, Fmax: 32, Fmin: 22})}
     color: ${theme.colors.linkNavFont};
     margin-bottom: 100px;
+
+    @media ${theme.media.mobile} {
+        margin-bottom: 60px;
+    }
 `
--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,8 +1,13 @@
 import styled from "styled-components";
 import {font} from "../styles/Common.ts";
+import {theme} from "../styles/Theme.ts";
 
 export const SectionTitle = styled.h2`
     text-align: center;
     ${font({weight: 700, Fmax: 48, Fmin: 30})}
     margin-bottom: 50px;
+    
+    @media ${theme.media.mobile} {
+        margin-bottom: 40px;
+    }
 `
