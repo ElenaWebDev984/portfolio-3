bea064248ed93261d59dfad156875cb568a6f80c
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-03-26 14:32:32 +0100

    created MixIn

--- src/styles/Common.ts
+++ src/styles/Common.ts
@@ -1,19 +1,23 @@
 import {theme} from "./Theme.ts";
 
-type FontPropsType = {
-    family?: string;
-    weight?: number;
-    color?: string;
-    lineHeight?: number;
-    Fmin?: number;
-    Fmax?: number;
+type FontType = {
+    family?: string
+    weight?: number
+    color?: string
+    lineHeight?: number
+    Fmin?: number
+    Fmax?: number
 }
 
-export const font = ({family,weight,color,lineHeight,Fmin,Fmax} : FontPropsType) => `
+export const font = ({family, weight, color, lineHeight, Fmin, Fmax} : FontType) => `
 font-family: ${family || "Poppins"};
 font-weight: ${weight || 400};
 color: ${color || theme.colors.h2h3Font};
 line-height: ${lineHeight || 1.2};
 
-font-size: clamp(${Fmin}px, calc( (100vw - 360px) / (1440 - 360) * (${Fmax} - ${Fmin}) + ${Fmin}px), ${Fmax}px);
+// font-size: calc((100vw -360px) / (1600 - 360) * (${Fmax} - ${Fmin}) + ${Fmin}px);
+
+font-size: clamp(${Fmin}px, calc( (100vw - 360px) / (1600 - 360) * (${Fmax} - ${Fmin}) + ${Fmin}px), ${Fmax}px);
+
+// font-size: clamp(${Fmin}px, calc( (100vw - 360px) / (1600 - 360) * (58 - 36) + 36px), ${Fmax}px);
 `
