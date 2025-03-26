1e9a2af946236718c74f67b0a38215469c3e001a
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-03-26 20:00:37 +0100

    added media and mixin

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -6,6 +6,7 @@ import {items} from "../header/Header.tsx";
 import {Container} from "../../components/Container.tsx";
 import {HeaderMenu} from "../header/headerMenu/HeaderMenu.tsx";
 import {theme} from "../../styles/Theme.ts";
+import {font} from "../../styles/Common.ts";
 
 
 export const Footer = () => {
@@ -27,11 +28,14 @@ export const Footer = () => {
 
 const StyledFooter = styled.footer`
     margin: 120px 0 60px;
+    
+    @media ${theme.media.tablet} {
+        margin: 60px 0;
+    }
 `
 
 const Copyright = styled.p`
-    font-weight: 400;
-    font-size: 18px;
+    ${font({weight: 400, Fmax: 18, Fmin: 14})}
     line-height: 1.4;
     color: ${theme.colors.linkNavFont};
 
@@ -43,6 +47,9 @@ const Copyright = styled.p`
         color: transparent;
         display: inline-block;
     }
+
+    @media ${theme.media.footer} {
+        margin: 0 auto;
 `
 
 const PseudoWrapper = styled.div`
