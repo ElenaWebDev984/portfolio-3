2583dcc8b706e0b4837de30efdad13b1618bc956
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-03-26 18:50:56 +0100

    added Container

--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -3,18 +3,21 @@ import {SectionTitle} from "../../../components/SectionTitle.tsx";
 import {Icon} from "../../../components/icon/Icon.tsx";
 import {Slider} from "../../../components/slider/Slider.tsx";
 import {FlexWrapper} from "../../../components/FlexWrapper.tsx";
+import {Container} from "../../../components/Container.tsx";
 
 
 export const Testimony = () => {
     return (
         <StyledTestimony>
-            <SectionTitle>Testimony</SectionTitle>
-            <FlexWrapper direction="column" alignItems="center">
-                <Icon iconId={'testimony'} viewBox={'-12 -12 50 50'}/>
-                <Slider
-                    text={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.'}
-                    name={'@ivan ivanow'}/>
-            </FlexWrapper>
+            <Container>
+                <SectionTitle>Testimony</SectionTitle>
+                <FlexWrapper direction="column" alignItems="center">
+                    <Icon iconId={'testimony'} viewBox={'-12 -12 50 50'}/>
+                    <Slider
+                        text={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.'}
+                        name={'@ivan ivanow'}/>
+                </FlexWrapper>
+            </Container>
         </StyledTestimony>
     );
 };
