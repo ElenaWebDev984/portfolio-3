57943fa2818eb202ec4956b1a5bf284d7db97d17
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-03-26 17:16:01 +0100

    added MixIn

--- src/components/SectionText.tsx
+++ src/components/SectionText.tsx
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 import {theme} from "../styles/Theme.ts";
+import {font} from "../styles/Common.ts";
 
 export const SectionText = styled.p`
     text-align: center;
-    font-weight: 400;
-    font-size: 32px;
+    ${font({weight: 400, Fmax: 32, Fmin: 24})}
     color: ${theme.colors.linkNavFont};
     margin-bottom: 100px;
 `
--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,8 +1,8 @@
 import styled from "styled-components";
+import {font} from "../styles/Common.ts";
 
 export const SectionTitle = styled.h2`
     text-align: center;
-    font-weight: 700;
-    font-size: 48px;
+    ${font({weight: 700, Fmax: 48, Fmin: 30})}
     margin-bottom: 50px;
 `
