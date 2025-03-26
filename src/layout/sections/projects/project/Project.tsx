8e43c30a5c79b1a5bb800d1ea180aa852c9edeeb
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-03-26 18:48:59 +0100

    added @media for imageWrapper

--- src/layout/sections/projects/project/Project.tsx
+++ src/layout/sections/projects/project/Project.tsx
@@ -68,6 +68,16 @@ const ImageWrapper = styled.div`
             opacity: 1;
         }
     }
+    
+    @media ${theme.media.tablet} {
+        &::before {
+            opacity: 1;
+        }
+
+        ${Button} {
+            opacity: 1;
+        }
+    }
 `
 
 const Image = styled.img`
