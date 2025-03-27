63edd462a33d476398919e64f962cdba9250f561
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-03-27 23:50:14 +0100

    created DesktopMenu

---
+++ src/layout/header/headerMenu/desktopMenu/DesktopMenu.tsx
@@ -0,0 +1,15 @@
+import {S} from './../HeaderMenu_Styles.ts'
+import * as React from "react";
+import {Menu} from "../menu/Menu.tsx";
+
+
+
+export const DesktopMenu: React.FC<{menuItems: Array<string>}> = (props: {menuItems: Array<string>}) => {
+    return (
+        <S.DesktopMenu>
+            <Menu menuItems={props.menuItems} />
+        </S.DesktopMenu>
+    );
+};
+
+
