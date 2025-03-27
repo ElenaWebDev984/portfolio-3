e1c055ad6de00639c7964f005400b74c1ed3c1cb
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-03-27 23:51:18 +0100

    added breakpoint

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,10 +1,11 @@
-import styled from "styled-components";
 import {Logo} from "../../components/logo/Logo.tsx";
 import {FlexWrapper} from "../../components/FlexWrapper.tsx";
 import {Container} from "../../components/Container.tsx";
 import {MenuContact} from "../../components/menu/MenuContact.tsx";
-import {HeaderMenu} from "./headerMenu/HeaderMenu.tsx";
-// import {MobileMenu} from "./mobileMenu/MobileMenu.tsx";
+import {DesktopMenu} from "./headerMenu/desktopMenu/DesktopMenu.tsx";
+import * as React from "react";
+import {MobileMenu} from "./headerMenu/mobileMenu/MobileMenu.tsx";
+import {S} from './Header_Styles.ts'
 
 
 export const items = ['Home', 'About', 'Tech Stack', 'Projects', 'Contact',]
@@ -15,34 +16,35 @@ export const items = ['Home', 'About', 'Tech Stack', 'Projects', 'Contact',]
 //     {iconId: 'telegram', href: '#', width: '30', height: '30', viewBox: '0 0 30 30'},
 // ]
 
-export const Header = () => {
+export const Header: React.FC = () => {
+    const [width, setWidth] = React.useState(window.innerWidth);
+    // const breakpoint = 768;
+    const breakpoint = 200;
+
+    React.useEffect(() => {
+        const handleWindowResize = () => setWidth(window.innerWidth);
+        window.addEventListener("resize", handleWindowResize);
+        return () => window.removeEventListener("resize", handleWindowResize);
+    }, []);
+
+
     return (
-        <StyledHeader>
-            <H1>Web Developer, React Developer, Frontend Developer, 2 years, Portfolio, UI/UX Designer, Web Development, Responsive Design, JavaScript, React, TypeScript, HTML, CSS, Bootstrap, Tailwind CSS, Figma, Adobe XD, User Experience, User Interface, Cross-Browser Compatibility, Version Control (Git), GitHub Pages Agile Methodologies, RESTful APIs, Mobile-First Design, Performance Optimization, SEO Best Practices, Accessibility (a11y), Wireframe, Prototyping, Graphic Design, Animation, CSS Preprocessors (Sass, Less), Frontend Frameworks</H1>
+        <S.Header>
+            <S.H1>Web Developer, React Developer, Frontend Developer, 2 years, Portfolio, UI/UX Designer, Web Development, Responsive Design, JavaScript, React, TypeScript, HTML, CSS, Bootstrap, Tailwind CSS, Figma, Adobe XD, User Experience, User Interface, Cross-Browser Compatibility, Version Control (Git), GitHub Pages Agile Methodologies, RESTful APIs, Mobile-First Design, Performance Optimization, SEO Best Practices, Accessibility (a11y), Wireframe, Prototyping, Graphic Design, Animation, CSS Preprocessors (Sass, Less), Frontend Frameworks</S.H1>
             <Container>
                 <FlexWrapper justifyContent="space-between" alignItems={'center'}>
                     <Logo/>
-                    <HeaderMenu menuItems={items}/>
-                    {/*<MobileMenu menuItems={items}/>*/}
+
+                    {width < breakpoint ? <MobileMenu menuItems={items}/>
+                                        : <DesktopMenu menuItems={items}/>}
+
                     <MenuContact />
                 </FlexWrapper>
             </Container>
-        </StyledHeader>
+        </S.Header>
     );
 };
 
 
-const StyledHeader = styled.header`
-    background: rgba(25, 25, 25, 0.9);
-    padding: 40px 0;
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    z-index: 999;
-`
 
-const H1 = styled.h1`
-    display: none;
-`
 
