2277c63e2e20726d78a5ad03a916b6ef300b1a2f
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-03-29 23:30:39 +0100

    changed styles for S

--- src/layout/sections/projects/tabMenu/TabMenu.tsx
+++ src/layout/sections/projects/tabMenu/TabMenu.tsx
@@ -1,57 +1,25 @@
-import styled from "styled-components";
-import {theme} from "../../../../styles/Theme.ts";
+import {Link, S} from './TabMenu_Styles.ts'
 
-type TabMenuPropsType = {
+export type TabMenuPropsType = {
     gap?: string
     marginBottom?: string
 }
 
 export const TabMenu = (props: {menuItems: Array<string>}) => {
     return (
-        <StyledTabMenu>
-            <TabList>
+        <S.TabMenu>
+            <S.TabList>
                 {props.menuItems.map((item, index) => {
-                    return  <ListItem key={index}>
+                    return  <S.ListItem key={index}>
                         <Link href="">{item}</Link>
-                    </ListItem>
+                    </S.ListItem>
                 })}
-            </TabList>
-        </StyledTabMenu>
+            </S.TabList>
+        </S.TabMenu>
     );
 };
 
-const StyledTabMenu = styled.nav<TabMenuPropsType>`
 
-    @media ${theme.media.mobile} {
-        display: none;
-    }
-`
-
-const TabList = styled.ul<TabMenuPropsType>`
-    display: flex;
-    justify-content: center;
-    gap: ${props => props.gap || '20px'};
-    margin-bottom: ${props => props.marginBottom || '40px'};
-    // TODO change gap and margin-bottom with props
-`
-
-const ListItem = styled.li`
-    
-`
-
-export const Link = styled.a`
-    font-weight: 400;
-    font-size: 14px;
-    text-transform: uppercase;
-    color: ${theme.colors.linkNavFont};
-    padding: 10px;
-    // border: 1px solid ${theme.colors.accentFont2};
-    // border-radius: 5px;
-    
-    &:hover {
-        background-color: ${theme.colors.accentFont2};
-    }
-`
 
 
 
