4a7a950410d2eaf0980fb180b4d2b3d02b942b26
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-03-30 00:22:59 +0100

    created Slider_Styles

---
+++ src/components/slider/Slider_Styles.ts
@@ -0,0 +1,55 @@
+import {theme} from "../../styles/Theme.ts";
+import styled from "styled-components";
+
+const Slider = styled.div`
+    max-width: 500px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const Slide = styled.div`
+    text-align: center;
+`
+
+const Text = styled.p`
+
+`
+
+const Name = styled.span`
+    font-family: 'DM Sans', sans-serif;
+    font-weight: 600;
+    font-size: 16px;
+    letter-spacing: 1px;
+    text-transform: uppercase;
+    margin: 22px 0 42px;
+    display: inline-block;
+`
+
+const Pagination = styled.div`
+    
+    span {
+        display: inline-block;
+        background-color: ${theme.colors.linkNavFont};
+        width: 7px;
+        height: 7px;
+        
+        & + span {
+            margin-left: 5px;
+        }
+        
+        &.active {
+            background-color: ${theme.colors.accentFont2};
+            width: 20px;
+        }
+    }
+`
+
+
+export const S = {
+    Slider,
+    Slide,
+    Text,
+    Name,
+    Pagination,
+}
