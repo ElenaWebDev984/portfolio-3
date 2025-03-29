401df76e9a34c05b441c96d12e80588f737141fe
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-03-29 23:29:52 +0100

    changed styles for S

--- src/layout/sections/projects/project/Project.tsx
+++ src/layout/sections/projects/project/Project.tsx
@@ -1,7 +1,6 @@
-import styled from "styled-components";
-import {theme} from "../../../../styles/Theme.ts";
+import {S} from './../Projects_Styles.ts'
 import {Button} from "../../../../components/Button.tsx";
-import {ProjectLinks} from "./ProjectLinks.tsx";
+import * as React from "react";
 
 
 type ProjectPropsType = {
@@ -12,108 +11,21 @@ type ProjectPropsType = {
 }
 
 
-export const Project = (props: ProjectPropsType) => {
+export const Project: React.FC<ProjectPropsType> = (props: ProjectPropsType) => {
     return (
-        <StyledProject>
-            <ImageWrapper>
-                <Image src={props.src} alt={props.alt}/>
+        <S.Project>
+            <S.ImageWrapper>
+                <S.Image src={props.src} alt={props.alt}/>
                 <Button width={'150px'} height={'40px'} padding={'7px 15px'} btnType={'primary'}>View Project</Button>
-            </ImageWrapper>
-            <Content>
-                <Title>{props.title}</Title>
-                <Text>{props.text}</Text>
-                <TechStacks><span>Tech stack : </span> HTML , JavaScript, SASS, React</TechStacks>
-                <ProjectLinks/>
-            </Content>
-        </StyledProject>
+            </S.ImageWrapper>
+            <S.Content>
+                <S.Title>{props.title}</S.Title>
+                <S.Text>{props.text}</S.Text>
+                <S.TechStacks><span>Tech stack : </span> HTML , JavaScript, SASS, React</S.TechStacks>
+                <S.ProjectLinks/>
+            </S.Content>
+        </S.Project>
     );
 };
 
-const StyledProject = styled.article`
-    background: ${theme.colors.projectBgc};
-    max-width: 375px;
-    width: 100%;
-    border-radius: 20px;
-`
-
-const ImageWrapper = styled.div`
-    position: relative;
-
-    ${Button} {
-        opacity: 0;
-        position: absolute;
-        left: 50%;
-        top: 50%;
-        transform: translateX(-50%);
-    }
-
-    &::before {
-        content: '';
-        position: absolute;
-        left: 0;
-        right: 0;
-        top: 0;
-        bottom: 0;
-        background: rgba(0, 0, 0, 0.3);
-        backdrop-filter: blur(4);
-        opacity: 0;
-    }
-
-    &:hover {
-        &::before {
-            opacity: 1;
-        }
-
-        ${Button} {
-            opacity: 1;
-        }
-    }
-    
-    @media ${theme.media.tablet} {
-        &::before {
-            opacity: 1;
-        }
-
-        ${Button} {
-            opacity: 1;
-        }
-    }
-`
-
-const Image = styled.img`
-    width: 100%;
-    height: 260px;
-    object-fit: cover;
-    border-top-left-radius: 20px;
-    border-top-right-radius: 20px;
-`
-
-const Content = styled.div`
-    padding: 30px;
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
-`
-
-const Title = styled.h1`
-    font-weight: 500;
-    font-size: 28px;
-`
-
-const Text = styled.p`
-    //margin: 20px 0 15px;
-    font-weight: 300;
-    font-size: 18px;
-    line-height: 144%;
-`
-
-const TechStacks = styled.p`
-    font-weight: 300;
-    font-size: 16px;
-    
-    span {
-        font-weight: 400;
-        font-size: 14px;
-    }
-`
 
