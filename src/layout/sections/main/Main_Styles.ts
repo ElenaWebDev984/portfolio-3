c8e169d9b300df801554b288b95599f3e321c4c7
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-03-30 03:23:33 +0200

    added pseudoPhoto

--- src/layout/sections/main/Main_Styles.ts
+++ src/layout/sections/main/Main_Styles.ts
@@ -9,6 +9,7 @@ const Main = styled.section`
     ${font({weight: 700, Fmax: 58, Fmin: 32})}
     color: ${theme.colors.mainSectionFont};
     letter-spacing: -0.02em;
+    overflow: clip;
 `
 
 const ContentMain = styled.div`
@@ -22,8 +23,8 @@ const ContentMain = styled.div`
 
 const MainTitle = styled.h2`
     white-space: nowrap;
-    ${font({weight: 700, Fmax: 58, Fmin: 32})}
 
+    ${font({weight: 700, Fmax: 58, Fmin: 32})}
     span {
         background: ${theme.colors.linearGradientFont};
         background-clip: text;
@@ -46,7 +47,7 @@ const ButtonWrapper = styled.div`
     gap: 40px;
 
     @media ${theme.media.mobile} {
-       gap: 20px;
+        gap: 20px;
     }
 `
 
@@ -84,38 +85,26 @@ const PhotoWrapper = styled.div`
         width: 280px;
         height: 280px;
     }
-    
-    &::before {
-        content: "";
-        width: 350px;
-        height: 350px;
-        border-radius: 50%;
 
-        // @media ${theme.media.desktop} {
-        //     width: 300px;
-        //     height: 300px;
-        // }
+    &::after {
+        content: "";
+        width: 630px;
+        height: 630px;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        background-image: url("./../../../../public/abstract.svg");
+        background-size: cover;
+        display: inline-block;
+        //pointer-events: none;
+        z-index: 1;
+        transform: rotate(360deg) translate(-50%, -50%);
+        opacity: 1;
+        border-width: 1px;
+        filter: brightness(0) invert(1) sepia(1) hue-rotate(180deg);
     }
 `
 
-// const AbstractSvg = styled.img`
-//     width: 630px;
-//     height: 630px;
-//     position: absolute;
-//     top: 50%;
-//     left: 50%;
-//     pointer-events: none;
-//     z-index: -1;
-//    transform: rotate(360deg) translate(-50%, -50%);
-//     opacity: 0.8;
-//     border-width: 1px;
-//     filter: brightness(0) invert(1) sepia(1) hue-rotate(180deg);
-
-//brightness(0) делает изображение полностью чёрным
-//invert(1) превращает чёрный цвет в белый
-//sepia(1) (эффект сепии), который превращает белый в коричневый
-//hue-rotate(200deg) поворачивает цветовой круг на 200 градусов, что даёт сине-фиолетовый оттенок
-// `
 
 export const S = {
     Main,
@@ -127,3 +116,9 @@ export const S = {
     Photo,
     PhotoWrapper,
 }
+
+
+//brightness(0) делает изображение полностью чёрным
+//invert(1) превращает чёрный цвет в белый
+//sepia(1) (эффект сепии), который превращает белый в коричневый
+//hue-rotate(200deg) поворачивает цветовой круг на 200 градусов, что даёт сине-фиолетовый оттенок
