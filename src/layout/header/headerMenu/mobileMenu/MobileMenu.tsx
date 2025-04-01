9381ebe58e650f75649a69f3e7f91aad085429c3
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-04-01 21:21:39 +0200

    added useState

--- src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
+++ src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
@@ -3,16 +3,19 @@ import {Menu} from "../menu/Menu.tsx";
 import * as React from "react";
 import {S} from './../HeaderMenu_Styles.ts'
 import {FlexWrapper} from "../../../../components/FlexWrapper.tsx";
+import {useState} from "react";
 
 
 export const MobileMenu: React.FC<{ menuItems: Array<string> }> = (props: { menuItems: Array<string> }) => {
+    const  [menuIsOpen, setMenuIsOpen] = useState(false)
+    const onBurgerBtnClick = () => {setMenuIsOpen(!menuIsOpen)}
     return (
         <S.MobileMenu>
-            <S.BurgerButton isOpen={true}>
+            <S.BurgerButton isOpen={menuIsOpen} onClick={onBurgerBtnClick}>
                 <span></span>
             </S.BurgerButton>
 
-            <S.MobileMenuPopup isOpen={true}>
+            <S.MobileMenuPopup isOpen={menuIsOpen} onClick={() => {setMenuIsOpen(false)}}>
                 <FlexWrapper direction="column" justifyContent={'center'} alignItems={'center'} gap={'50px'}>
                     <Menu menuItems={props.menuItems}/>
                     <MobileMenuContact/>
