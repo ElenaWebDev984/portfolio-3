7d8f6f65f1364777b54642566a4c05fc760ce361
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-04-20 19:51:47 +0200

    added Slider from library

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,6 +1,6 @@
-import {S} from './Slider_Styles.ts'
-import {FlexWrapper} from "../FlexWrapper.tsx";
-import * as React from "react";
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+import {S} from './Slider_Styles';
 
 
 type SliderPropsType = {
@@ -8,21 +8,59 @@ type SliderPropsType = {
     name?: string
 }
 
-export const Slider: React.FC<SliderPropsType> = (props: SliderPropsType) => {
+const Slide = (props: SliderPropsType) => {
     return (
-        <S.Slider>
-            <FlexWrapper>
-                <S.Slide>
-                    <S.Text>{props.text}</S.Text>
-                    <S.Name>{props.name}</S.Name>
-                </S.Slide>
-            </FlexWrapper>
-            <S.Pagination>
-                <span></span>
-                <span className={'active'}></span>
-                <span></span>
-            </S.Pagination>
-        </S.Slider>
-    );
-};
+        <S.Slide>
+            <S.Text>{props.text}</S.Text>
+            <S.Name>@{props.name}</S.Name>
+        </S.Slide>
+    )
+}
 
+const items = [
+   <Slide name={'@ivan ivanow'}
+          text={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.'}/>,
+   <Slide name={'igor igorev'}
+          text={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.'}/>,
+   <Slide name={'anton sokolov'}
+          text={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.'}/>,
+];
+
+export const Slider = () => (
+    <S.Slider>
+        <AliceCarousel
+            mouseTracking
+            items={items}
+        />
+    </S.Slider>
+);
+
+
+// import {S} from './Slider_Styles.ts'
+// import {FlexWrapper} from "../FlexWrapper.tsx";
+// import * as React from "react";
+//
+//
+// type SliderPropsType = {
+//     text?: string
+//     name?: string
+// }
+//
+// export const Slider: React.FC<SliderPropsType> = (props: SliderPropsType) => {
+//     return (
+//         <S.Slider>
+//             <FlexWrapper>
+//                 <S.Slide>
+//                     <S.Text>{props.text}</S.Text>
+//                     <S.Name>{props.name}</S.Name>
+//                 </S.Slide>
+//             </FlexWrapper>
+//             <S.Pagination>
+//                 <span></span>
+//                 <span className={'active'}></span>
+//                 <span></span>
+//             </S.Pagination>
+//         </S.Slider>
+//     );
+// };
+//
