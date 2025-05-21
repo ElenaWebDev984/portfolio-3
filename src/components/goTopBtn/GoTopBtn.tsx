f8aa780259b048e1a342e4fda419b114f281a161
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-05-21 03:59:51 +0200

    added goTopBtn

--- src/components/goTopBtn/GoTopBtn.tsx
+++ src/components/goTopBtn/GoTopBtn.tsx
@@ -1,13 +1,30 @@
 import styled from "styled-components";
 import {Icon} from "../icon/Icon.tsx";
 import {animateScroll as scroll} from "react-scroll";
+import {useEffect, useState} from "react";
 
 
 export const GoTopBtn = () => {
+    const [showBtn, setShowBtn] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 200) {
+                setShowBtn(true);
+            } else {
+                setShowBtn(false);
+            }
+        });
+    }, [])
+
     return (
-        <StyledGoTopBtn onClick={() => {scroll.scrollToTop()}}>
-            <Icon iconId={'arrow-top'} width="40" height="40" viewBox={'0 0 100 100'}/>
-        </StyledGoTopBtn>
+        <>
+            {showBtn && (
+                <StyledGoTopBtn onClick={() => {scroll.scrollToTop()}}>
+                    <Icon iconId={'arrow-top'} width="40" height="40" viewBox={'0 0 100 100'}/>
+                </StyledGoTopBtn>
+            )}
+        </>
     );
 };
 
