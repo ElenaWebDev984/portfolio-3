ba5aea667a674486fb7183027b02252b9576e6d4
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-05-21 02:59:28 +0200

    added sendEmail

--- src/layout/sections/contact/Contact.tsx
+++ src/layout/sections/contact/Contact.tsx
@@ -5,46 +5,47 @@ import {FlexWrapper} from "../../../components/FlexWrapper.tsx";
 import { Container } from "../../../components/Container.tsx";
 import {MenuSocial} from "./MenuSocial.tsx";
 import emailjs from '@emailjs/browser';
-import {ElementRef, useRef, FormEvent} from "react";
+import {ElementRef, FormEvent, useRef} from "react";
+
 
 export const Contact = () => {
     const form = useRef<ElementRef<'form'>>(null);
 
-    const sendEmail = async (e: FormEvent<HTMLFormElement>) => {
+    const sendEmail = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
-        console.log("Form submitted!");
         if (!form.current) return;
 
-        console.log("Sending email...");
-        try {
-            const result = await emailjs.sendForm(
-                'service_p2q2ayn',
-                'template_4nggg5o',
-                form.current,
-                'lMxnNoRnNKj7l-yCR'
+        emailjs
+            .sendForm('service_p2q2ayn', 'template_4nggg5o', form.current, {
+                publicKey: 'lMxnNoRnNKj7l-yCR',
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                },
             );
-            console.log("Email sent!", result);
-        } catch (error) {
-            console.error("Email send error:", error);
-        }
-    };
+        e.currentTarget.reset();
+    }
 
-    return (
-        <S.Contact id='contact'>
-            <Container>
-                <SectionTitle>Get in touch</SectionTitle>
-                <FlexWrapper direction={"column"} alignItems={'center'}>
-                    <S.Form ref={form} onSubmit={sendEmail}>
-                        <S.Field required placeholder={'Name'} name={'user_name'}/>
-                        <S.Field required placeholder={'Email'} type='email' name={'email'}/>
-                        <S.Field required placeholder={'Subject'} name={'subject'}/>
-                        <S.Field required as={'textarea'} placeholder={'Message'} name={'message'}/>
-                        <Button type={'submit'} width={'100%'} padding={'7px 15px'} marginBottom={'50px'} btnType={'primary'}>Send Message</Button>
-                    </S.Form>
-                    <MenuSocial />
-                </FlexWrapper>
-            </Container>
-        </S.Contact>
-    );
-};
+        return (
+            <S.Contact id='contact'>
+                <Container>
+                    <SectionTitle>Get in touch</SectionTitle>
+                    <FlexWrapper direction={"column"} alignItems={'center'}>
+                        <S.Form ref={form} onSubmit={sendEmail}>
+                            <S.Field required placeholder={'Name'} name={'user_name'}/>
+                            <S.Field required placeholder={'Email'} type='email' name={'email'}/>
+                            <S.Field required placeholder={'Subject'} name={'subject'}/>
+                            <S.Field required as={'textarea'} placeholder={'Message'} name={'message'}/>
+                            <Button type={'submit'} width={'100%'} padding={'7px 15px'} marginBottom={'50px'} btnType={'primary'}>Send Message</Button>
+                        </S.Form>
+                        <MenuSocial />
+                    </FlexWrapper>
+                </Container>
+            </S.Contact>
+        );
+    };
